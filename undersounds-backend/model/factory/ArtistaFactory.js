5f9d433770e741b4fe6f75eeee137a3b98a1cdc0
leotorol <leotorolr@gmail.com>
2025-04-08 09:58:13 +0200

    ArtistFactory arreglado

--- undersounds-backend/model/factory/ArtistaFactory.js
+++ undersounds-backend/model/factory/ArtistaFactory.js
@@ -1,4 +1,5 @@
 const { Artist } = require('../models/Artistas');
+const ArtistDTO = require('../dto/ArtistDTO');
 
 class ArtistaFactory {
     // Crea una instancia del modelo Artist desde datos crudos
@@ -9,14 +10,20 @@ class ArtistaFactory {
             genre: data.genre,
             bio: data.bio || '',
             banner: data.banner || '',
-            seguidores: data.seguidores || 0,
+            seguidores: data.seguidores || Math.floor(Math.random() * (2000000 - 4000 + 1)) + 4000,
             ubicacion: data.ubicacion || '',
             idAlbum: data.idAlbum || [],
             ConcertColection: data.ConcertColection || [],
             createdAt: data.createdAt || new Date(),
             updatedAt: data.updatedAt || new Date()
         });
+
+        
     }
+        static create(artist) {
+            return new ArtistDTO(artist);
+        }
+    
 }
 
 module.exports = ArtistaFactory;
